617644f7be5318ffebc47fe7e59ffa5fad3d9149
tronprogram <bassgamer1234@outlook.es>
2023-02-26 13:30:46 -0500

    new file:   da-js-games/crazysweeper/app.js 	new file:   da-js-games/crazysweeper/index.html 	new file:   da-js-games/crazysweeper/styles.css

---
+++ da-js-games/crazysweeper/app.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const grid=document.querySelector('.grid')
+    let width=10
+    let squares=[]
+    let bombAmount=20
+    // cuadricula
+    function createBoard(){
+        // aleatorizar bombas time
+        const bombsArray=Array(bombAmount).fill('bomb')
+        const emptyArray=Array(width*width-bombAmount).fill('valid')
+        const gameArray=emptyArray.concat(bombsArray)
+        const shuffledArray=gameArray.sort(()=>Math.random()-0.5)
+
+        
+        for (let i=0; i<width*width; i++){
+            const square=document.createElement('div')
+            square.setAttribute('id', i)
+            square.classList.add(shuffledArray[i])
+            grid.appendChild(square)
+            squares.push(square)
+        }
+    }
+    createBoard()
+})
